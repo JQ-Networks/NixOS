eff1921fe1cbf8eff55e7f3d68ce9b991f7a0a1b
Curtis Jiang <jqqqqqqqqqq@gmail.com>
2022-02-27 21:23:50 +0000

    update mach-nix

--- flake.nix
+++ flake.nix
@@ -3,18 +3,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     indexyz.url = "github:X01A/nixos";
-    mach-nix.url = "github:DavHau/mach-nix?ref=3.3.0";
+    mach-nix.url = "github:DavHau/mach-nix?ref=3.4.0";
   };
   outputs = inputs@{ self, nixpkgs, unstable, indexyz, mach-nix, ... }:
     {
       overlays = [
         (
           final: prev: {
-            unstable = (import unstable { system = final.system; allowUnfree = true;});
+            unstable = (import unstable { system = final.system; config.allowUnfree = true;});
           }
         )
         # (final: prev: (indexyz.overlay.${final.system} final prev))
-        (final: prev: { mach-nix = import mach-nix { pkgs = import nixpkgs {}; pypiDataRev = "fc8e41f3c2e219e644e3784b68bda8dd31a29178"; pypiDataSha256 = "0s8as6zm7kci1y78cw5f69la0njk9am3yj2y8g710nvcg60pr3bd"; }; })
+        # https://github.com/DavHau/pypi-deps-db
+
+        (final: prev: { mach-nix = import mach-nix { pkgs = import nixpkgs {}; pypiDataRev = "42a6f18b7abafe97d73cc8045d6357915c5ecf77"; pypiDataSha256 = "sha256-uKf8bMR6prNIuvXtoNyKH7gfd1pBAeHQ6l0i0SSh9IA="; }; })
         indexyz.overlay."x86_64-linux"
         (
           final: prev: {
@@ -25,7 +27,7 @@
       ];
       packages."x86_64-linux" = 
       let 
-        pkgs = import nixpkgs { system = "x86_64-linux"; allowUnfree = true; };
+        pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
       in
         import ./pkgs { nixpkgs = pkgs; };
       nixosModules = {
