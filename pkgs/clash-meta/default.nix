81fbe4a1830070e56d2eef76d5b342406e0c4c5c
Curtis Jiang <jqqqqqqqqqq@gmail.com>
2022-12-30 06:35:49 +0000

    add clash.meta

---
+++ pkgs/clash-meta/default.nix
@@ -0,0 +1,26 @@
+{ fetchFromGitHub, buildGoModule, lib, source }:
+buildGoModule rec {
+  inherit (source) src pname version;
+
+  vendorSha256 = "sha256-QdrhMrDPYc5RhuZp3iNdlKGuXCo+oRb7rNEx2gANTz4=";
+  doCheck = false;
+
+  buildPhase = ''
+    buildFlagsArray=(-v -trimpath -tags "with_gvisor" -p $NIX_BUILD_CORES -ldflags='-X "github.com/Dreamacro/clash/constant.Version=${version}" -s -w -buildid=')
+    runHook preBuild
+    go build "''${buildFlagsArray[@]}" -o Clash.Meta
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    install -Dm755 Clash.Meta -t $out/bin
+  '';
+
+  meta = with lib; {
+    description = "Another Clash Kernel.";
+    homepage = "https://github.com/MetaCubeX/Clash.Meta";
+    license = licenses.gpl3;
+    platforms = with platforms; linux ++ darwin;
+  };
+
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,4 +7,5 @@ in
   mtg = callPackage ./mtg { lib = nixpkgs.lib; source = nvfetcherOut.mtg; };
   xray = callPackage ./xray { lib = nixpkgs.lib; buildGoModule = buildGo118Module; source = nvfetcherOut.xray; };
   sing-box = callPackage ./sing-box { lib = nixpkgs.lib; buildGoModule = buildGo118Module; source = nvfetcherOut.sing-box; };
+  clash-meta = callPackage ./clash-meta { lib = nixpkgs.lib; buildGoModule = buildGo118Module; source = nvfetcherOut.clash-meta; };
 }
